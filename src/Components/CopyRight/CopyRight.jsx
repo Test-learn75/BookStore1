e21aa012f4f22220762494ac64da58a41c5f9773
William Tran <64305892+wilou85@users.noreply.github.com>
2024-11-07 11:14:06 +0100

    v2

--- src/Components/CopyRight/CopyRight.jsx
+++ src/Components/CopyRight/CopyRight.jsx
@@ -8,7 +8,7 @@ export default function CopyRight() {
   return (
     <div className='footer-copyright'>
         <div className="container copyright-container">
-            <p>© 2024 Zapterify. All rights reserved.</p>
+            <p>© 2024 Qualihom All rights reserved.</p>
             <div className="footer-socials">
                 {
                     FootersLinksData.socials.map((item,index)=>{
